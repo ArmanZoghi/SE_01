4c4e849de623d33f78abef99fb76a5473978fd4b
Arman Zoghi <armanzoghi@Armans-MacBook-Air-2.local>
2022-05-11 17:02:38 +0200

    ex2

---
+++ ex2/main.js
@@ -0,0 +1,77 @@
+const isGameWon = false
+const table = [
+    true, true, true, true, true,
+    true, true, true, true, true,
+    true, true, true, true, true,
+    true, true, true, true, true,
+    true, true, true, true, true,
+]
+const tableDim = Math.sqrt(table.length)
+
+const checkWon = () => {
+    const isWin = table.indexOf(true)
+    if(isWin === -1) {
+        isGameWon= true
+    }
+}
+
+const makeToggle = (index) => {
+    table[index] = !table[index]
+
+    if(index < tableDim) {
+        if(index === 0) {
+            table[index + 1] = !table[index + 1]
+            table[index + tableDim] = !table[index + tableDim]
+        } else if(index === tableDim - 1){
+            table[index - 1] = !table[index - 1] 
+            table[index + tableDim] = !table[index + tableDim]
+        } else {
+            table[index + 1] = !table[index + 1]
+            table[index - 1] = !table[index - 1] 
+            table[index + tableDim] = !table[index + tableDim]
+        }
+    } else if(index >= (tableDim - 1) * tableDim  && index < tableDim * tableDim){
+        if(index === (tableDim - 1) * tableDim) {
+            table[index + 1] = !table[index + 1]
+            table[index - tableDim] = !table[index - tableDim]
+        } else if(tableDim * tableDim - 1) {
+            table[index - 1] = !table[index - 1] 
+            table[index - tableDim] = !table[index - tableDim]
+        } else {
+            table[index + 1] = !table[index + 1]
+            table[index - 1] = !table[index - 1] 
+            table[index - tableDim] = !table[index - tableDim]
+        }
+    } else {
+        if(index % tableDim === 0) {
+            table[index + 1] = !table[index + 1]
+            table[index - tableDim] = !table[index - tableDim]
+            table[index + tableDim] = !table[index + tableDim]
+        } else if(index % tableDim === tableDim - 1) {
+            table[index - 1] = !table[index - 1]
+            table[index - tableDim] = !table[index - tableDim]
+            table[index + tableDim] = !table[index + tableDim]
+        } else {
+            table[index + 1] = !table[index + 1]
+            table[index - 1] = !table[index - 1]
+            table[index - tableDim] = !table[index - tableDim]
+            table[index + tableDim] = !table[index + tableDim]
+        }
+    }
+}
+
+const toggle = () => {
+    console.log(table)
+    let toggleIndex = prompt('index')
+    makeToggle(parseInt(toggleIndex))
+    alert(table)
+    checkWon()
+}
+do {
+    toggle()
+}
+while(!isGameWon)
+
+if(isGameWon) {
+    console.log('You won')
+}
